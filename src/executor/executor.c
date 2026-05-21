a56f32efe49deb14552b5c8a0e8bf28a8102f123
Bala Andegue <balaandeguefrancoislionnel@gmail.com>
2026-05-21 13:55:06 +0100

    feat(executor): fork child process and exec external commands

---
+++ src/executor/executor.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#include "executor.h"
+
+static int run_external(char **argv, int argc)
+{
+    (void)argc;
+    pid_t pid = fork();
+    if (pid < 0) {
+        perror("fork");
+        return 1;
+    }
+    if (pid == 0) {
+        execvp(argv[0], argv);
+        perror(argv[0]);
+        exit(127);
+    }
+    int status;
+    if (waitpid(pid, &status, 0) == -1) {
+        perror("waitpid");
+        return 1;
+    }
+    if (WIFEXITED(status))
+        return WEXITSTATUS(status);
+    if (WIFSIGNALED(status))
+        return 128 + WTERMSIG(status);
+    return 1;
+}
+
+int execute(char **argv, int argc, t_shell *sh)
+{
+    (void)sh;
+    if (argc == 0 || argv[0] == NULL)
+        return 0;
+    return run_external(argv, argc);
+}
---
+++ src/executor/executor.h
@@ -0,0 +1,8 @@
+#ifndef EXECUTOR_H
+#define EXECUTOR_H
+
+#include "../../include/minishell.h"
+
+int execute(char **argv, int argc, t_shell *sh);
+
+#endif /* EXECUTOR_H */
